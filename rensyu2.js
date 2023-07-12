9cac1e35e4682b6cdee86e05bc71aaca186d0d18
JIKim0303 <j_inoue9133@outlook.com>
2023-07-12 19:39:12 +0900

    second

--- rensyu2.js
+++ rensyu2.js
@@ -2,11 +2,17 @@ const sliderimg1 = document.getElementById('sliderimg1');
 const sliderimg2 = document.getElementById('sliderimg2');
 const sliderimg3 = document.getElementById('sliderimg3');
 
-setTimeout(() => {
-    sliderimg1.classList.add('activeleft');
-    // sliderimg1.classList.remove('activeleft');
-    sliderimg1.classList.add('activeright');
-    // sliderimg1.classList.remove('activeright');
-    sliderimg1.classList.add('activemiddle');
-    // sliderimg1.classList.remove('activemiddle');
-    }, 3000);
+// window.onload = function slideimg() {
+//     setInterval(() => {
+//     sliderimg1.classList.add('activeleft');
+//     sliderimg1.classList.remove('activeleft');
+//     sliderimg1.classList.add('activeright');
+//     sliderimg1.classList.remove('activeright');
+//     sliderimg1.classList.add('activemiddle');
+//     sliderimg1.classList.remove('activemiddle');
+//     }, 1000)
+// }
+
+// var photos = document.getElementById('photos');
+// var photosli = document.getElemengById('photosli');
+// var li_count = photosli.length;
